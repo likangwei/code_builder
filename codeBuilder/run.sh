cd13f483263784a3e4599dfaf2103fec7cd85f4b
kevin <lovekangge@163.com>
2018-04-19 15:39:44 +0800

    Update run.sh

--- codeBuilder/run.sh
+++ codeBuilder/run.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/go-1.9/bin/:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games
+cd /var/www/code_builder/codeBuilder/
 FLASK_APP=main.py flask run
